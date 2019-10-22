9f380f9179266ba8750bf4a0578f88d6e0d187cb
Maria Teguiani <mariateguiani@gmail.com>
2019-10-22 16:10:13 +0100

    Remove ar drone references (with mb4617)

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,6 +1,5 @@
 #! /bin/sh
 
-sudo apt-get update
 sudo apt-get -y update
 
 sudo apt-get docker docker-engine docker.io
@@ -9,7 +8,7 @@ sudo add-apt-repository \
     $(lsb_release -cs) \
     stable"
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo apt-get update
+sudo apt-get -y update
 sudo apt-get install -y docker-ce
 
 sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
@@ -19,7 +18,6 @@ sudo apt install -y docker-compose
 sudo groupadd docker
 sudo gpasswd -a $USER docker
 
-docker pull ros:kinetic-robot
 docker build -t xdrone .
 
 newgrp docker
